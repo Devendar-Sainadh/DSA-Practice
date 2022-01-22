1bdefddaf8249663b2381f975e2afbb15ec87770
Devendar Sainadh Gorla <84167105+Devendar-Sainadh@users.noreply.github.com>
2022-01-22 11:39:54 +0530

    Update hcf.cpp

--- Maths/hcf.cpp
+++ Maths/hcf.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
     
@@ -15,3 +16,48 @@ int main()
     cout<<res;
     return 0;
 }
+*/
+
+//Highly Optimised solution
+
+#include <iostream>
+using namespace std;
+
+int gcd(int a,int b){
+    if(b==0){
+        return a;
+    }
+    else{
+        return gcd(b,a%b);
+    }
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int x=gcd(a,b);
+    cout<<x;
+    return 0;
+}
+
+// Optimised solution
+/*
+#include <iostream>
+using namespace std;
+    
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    while(a!=b){
+        if(a>b){
+            a=a-b;
+        }
+        else{
+            b=b-a;
+        }
+    }
+    cout<<a;
+    return 0;
+}
+*/
