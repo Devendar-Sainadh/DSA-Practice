9cd92f703f47977a116944a559c91971e3bbdec2
Devendar Sainadh Gorla <84167105+Devendar-Sainadh@users.noreply.github.com>
2022-01-27 13:25:02 +0530

    Add files via upload

---
+++ Arrays/union.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int main()
+{
+int m,n;
+cin>>m>>n;
+int a[m];
+int b[n];
+vector<int> c;
+for(int i=0;i<m;i++){
+cin>>a[i];
+}
+
+for(int i=0;i<n;i++){
+cin>>b[i];
+}
+
+int i=0;
+int j=0;
+
+while (i<m && j<n)
+{
+    if(a[i]<b[j]){
+        c.push_back(a[i++]);
+    }
+    else if(b[j]<a[i]){
+       c.push_back(b[j++]);    }
+    else{
+      c.push_back(a[i]);
+        i++;
+        j++;
+    }
+}
+
+for(;i<m;i++){
+    c.push_back(a[i]);
+}
+for(;j<n;j++){
+    c.push_back(b[j]);
+}
+
+for(int k=0;k<c.size();k++){
+cout<<c[k]<<" ";
+}
+    return 0;
+}
